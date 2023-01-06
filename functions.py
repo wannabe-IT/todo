6b40265d261eadddcfc1a06215db90ed896ab994
mib <jajan151@seznam.cz>
2023-01-06 17:02:47 +0100

    added separate functions file

---
+++ functions.py
@@ -0,0 +1,13 @@
+def get_todos(filepath="files/todos.txt"):
+    """ Read a text file and return the list of
+    to-do items.
+    """
+    with open(filepath, "r") as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath="files/todos.txt"):
+    """ Write the to-do items list in the text file."""
+    with open(filepath, "w") as file_local:
+        file_local.writelines(todos_arg)
--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-def get_todos(filepath="files/todos.txt"):
-    """ Read a text file and return the list of
-    to-do items.
-    """
-    with open(filepath, "r") as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
-
-
-def write_todos(todos_arg, filepath="files/todos.txt"):
-    """ Write the to-do items list in the text file."""
-    with open(filepath, "w") as file_local:
-        file_local.writelines(todos_arg)
-
+import functions
 
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
@@ -20,15 +7,15 @@ while True:
     if user_action.startswith("add"):
         todo = user_action[4:]
 
-        todos = get_todos()
+        todos = functions.get_todos()
 
         todos.append(todo + '\n')
 
-        write_todos(todos)
+        functions.write_todos(todos)
 
     elif user_action.startswith("show"):
 
-        todos = get_todos()
+        todos = functions.get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip("\n")
@@ -40,12 +27,12 @@ while True:
             number = int(user_action[5:])
             number = number - 1
 
-            todos = get_todos()
+            todos = functions.get_todos()
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + "\n"
 
-            write_todos(todos)
+            functions.write_todos(todos)
 
         except ValueError:
             print("Your command is not valid.")
@@ -55,12 +42,12 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos()
+            todos = functions.get_todos()
             index = number - 1
             todo_to_remove = todos[index].strip("\n")
             todos.pop(index)
 
-            write_todos(todos)
+            functions.write_todos(todos)
 
             message = f"Todo {todo_to_remove} was removed from the list."
             print(message)
