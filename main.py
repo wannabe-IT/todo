38f4b2789bf38df7223a4eaf3a1eb5f7e57c2672
mib <jajan151@seznam.cz>
2023-01-06 16:50:25 +0100

    features added

--- main.py
+++ main.py
@@ -1,31 +1,34 @@
-def get_todos(filepath):
+def get_todos(filepath="files/todos.txt"):
+    """ Read a text file and return the list of
+    to-do items.
+    """
     with open(filepath, "r") as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(filepath, todos_arg):
+def write_todos(todos_arg, filepath="files/todos.txt"):
+    """ Write the to-do items list in the text file."""
     with open(filepath, "w") as file_local:
         file_local.writelines(todos_arg)
 
 
 while True:
-
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
     if user_action.startswith("add"):
         todo = user_action[4:]
 
-        todos = get_todos(filepath="files/todos.txt")
+        todos = get_todos()
 
         todos.append(todo + '\n')
 
-        write_todos("files/todos.txt", todos)
+        write_todos(todos)
 
     elif user_action.startswith("show"):
 
-        todos = get_todos("files/todos.txt")
+        todos = get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip("\n")
@@ -37,12 +40,12 @@ while True:
             number = int(user_action[5:])
             number = number - 1
 
-            todos = get_todos("files/todos.txt")
+            todos = get_todos()
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + "\n"
 
-            write_todos("files/todos.txt", todos)
+            write_todos(todos)
 
         except ValueError:
             print("Your command is not valid.")
@@ -52,12 +55,12 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos("files/todos.txt")
+            todos = get_todos()
             index = number - 1
             todo_to_remove = todos[index].strip("\n")
             todos.pop(index)
 
-            write_todos("files/todos.txt", todos)
+            write_todos(todos)
 
             message = f"Todo {todo_to_remove} was removed from the list."
             print(message)
