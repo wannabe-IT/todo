a07eb84b7b6d24184417e3598557e3a9815e18c6
mib <jajan151@seznam.cz>
2023-01-13 02:17:50 +0100

    img

--- gui.py
+++ gui.py
@@ -7,13 +7,13 @@ sg.theme("Black")
 clock = sg.Text("", key="clock")
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo", size=[36, 1])
-add_button = sg.Button(size=2, image_source="add.png", mouseover_colors="LightBlue2",
+add_button = sg.Button(size=2, image_source="./img/add.png", mouseover_colors="LightBlue2",
                        tooltip="Add Todo", key="Add")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=[35, 10])
-edit_button = sg.Button(size=2, image_source="edit.png", mouseover_colors="LightBlue2",
+edit_button = sg.Button(size=2, image_source="./img/edit.png", mouseover_colors="LightBlue2",
                         tooltip="Edit Todo", key="Edit")
-complete_button = sg.Button(size=2, image_source="complete.png", mouseover_colors="LightBlue2",
+complete_button = sg.Button(size=2, image_source="./img/complete.png", mouseover_colors="LightBlue2",
                             tooltip="Complete Todo", key="Complete")
 
 exit_button = sg.Button("Exit")
