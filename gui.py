4d630c078128b13b658ddda317b8da019744728f
mib <jajan151@seznam.cz>
2023-01-13 01:35:49 +0100

    Added img buttons

--- gui.py
+++ gui.py
@@ -6,12 +6,16 @@ sg.theme("Black")
 
 clock = sg.Text("", key="clock")
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button("Add")
+input_box = sg.InputText(tooltip="Enter todo", key="todo", size=[36, 1])
+add_button = sg.Button(size=2, image_source="add.png", mouseover_colors="LightBlue2",
+                       tooltip="Add Todo", key="Add")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
-                      enable_events=True, size=[45, 10])
-edit_button = sg.Button("Edit")
-complete_button = sg.Button("Complete")
+                      enable_events=True, size=[35, 10])
+edit_button = sg.Button(size=2, image_source="edit.png", mouseover_colors="LightBlue2",
+                        tooltip="Edit Todo", key="Edit")
+complete_button = sg.Button(size=2, image_source="complete.png", mouseover_colors="LightBlue2",
+                            tooltip="Complete Todo", key="Complete")
+
 exit_button = sg.Button("Exit")
 
 window = sg.Window("My To-Do App",
