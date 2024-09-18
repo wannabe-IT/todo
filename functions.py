e51028f2910c19119517e4f00525aaf4c5f78807
mib <jajan151@seznam.cz>
2024-09-18 04:49:29 +0200

    Add sorting buttons and save confirmation to GUI
    Enhanced the GUI by adding up and down buttons for sorting todos and a save button with a confirmation prompt. Improved text processing in functions.py by removing empty lines and preparing for todo enumeration and moving functionalities.

--- functions.py
+++ functions.py
@@ -1,5 +1,5 @@
-FILEPATH = "todos.txt"
 
+FILEPATH = "todos.txt"
 
 def get_todos(filepath=FILEPATH):
     """ Read a text file and return the list of
@@ -7,15 +7,41 @@ def get_todos(filepath=FILEPATH):
     """
     with open(filepath, "r") as file_local:
         todos_local = file_local.readlines()
+        todos_local = remove_empty_lines(todos_local)
     return todos_local
-
 
 def write_todos(todos_arg, filepath=FILEPATH):
     """ Write the to-do items list in the text file."""
     with open(filepath, "w") as file_local:
         file_local.writelines(todos_arg)
 
+def remove_empty_lines(todos_list):
+    todoss=[]
+    for todo in todos_list:
+        if todo != "\n":
+            todoss.append(todo)
+    todos_list = todoss
+    return todos_list
+
+def move_in_list_up(enumerated_todoes):
+    pass
+
+
+def enumerate_todo_list():
+    enumerated_todos = []
+    todoes = get_todos(filepath=FILEPATH)
+    for first_char in todoes:
+        if first_char[0] != 1:
+            enumerated_todoes = [f"{index + 1}. {todo}" for index, todo in enumerate(todoes)]
+            for todo in enumerated_todoes:
+                print(todo.split('.', 1)[1].strip())
+                enumerated_todos.append(todo)
+            write_todos(enumerated_todos, filepath=FILEPATH)
+            break
+        else:
+            break
+
 
 if __name__ == "__main__":
-    print("Hello")
-    print(get_todos())
+    #move_in_list_up()
+    pass
--- gui.py
+++ gui.py
@@ -1,37 +1,51 @@
+from asyncio import timeout
+
 import functions
 import PySimpleGUI as sg
 import time
+
 
 sg.theme("Black")
 
 clock = sg.Text("", key="clock")
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo", key="todo", size=[36, 1])
+
+up_button = sg.Button(size=2, mouseover_colors="LightBlue2",key="Up", image_source="./img/up.png", image_size=(20, 20),)
+down_button = sg.Button(size=2, mouseover_colors="LightBlue2",key="Down", image_source="./img/down.png")
+input_box = sg.InputText(tooltip="Enter todo", key="todo", size=(36, 1))
 add_button = sg.Button(size=2, image_source="./img/add.png", mouseover_colors="LightBlue2",
                        tooltip="Add Todo", key="Add")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
-                      enable_events=True, size=[35, 10])
+                      enable_events=True, size=(35, 10))
 edit_button = sg.Button(size=2, image_source="./img/edit.png", mouseover_colors="LightBlue2",
-                        tooltip="Edit Todo", key="Edit")
+                        tooltip="Edit Todo", key="Edit", )
 complete_button = sg.Button(size=2, image_source="./img/complete.png", mouseover_colors="LightBlue2",
                             tooltip="Complete Todo", key="Complete")
-
+save_button = sg.Button("Save")
 exit_button = sg.Button("Exit")
 
 window = sg.Window("My To-Do App",
                    layout=[[clock],
                            [label],
                            [input_box, add_button],
-                           [list_box, edit_button, complete_button],
-                           [exit_button]],
+                           [list_box], [down_button, up_button, edit_button, complete_button,save_button, exit_button]],
                    font=("Helvetica", 20))
+
+todos_original = functions.get_todos()
+todos_completed = []
+todos = functions.get_todos()
 
 while True:
     event, values = window.read(timeout=200)
     window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
+
     match event:
+        case "Up":
+            todo_index = values["todo"]
+            #functions.move_in_list_up()
+        case "Down":
+            pass
         case "Add":
-            todos = functions.get_todos()
             new_todo = values["todo"] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
@@ -39,22 +53,25 @@ while True:
         case "Edit":
             try:
                 todo_to_edit = values["todos"][0]
-                new_todo = values["todo"]
+                new_todo = values["todo"] + "\n"
                 todos = functions.get_todos()
                 index = todos.index(todo_to_edit)
-                todos[index] = new_todo + "\n"
+                todos[index] = new_todo
                 functions.write_todos(todos)
                 window["todos"].update(values=todos)
             except IndexError:
-                sg.popup("Please select an item first.", font=("Helvetica", 20))
+                sg.popup("Please select an item first.", font=("Helvetica", 20),
+                         keep_on_top=True, modal=True)
         case "Complete":
             try:
                 todo_to_complete = values["todos"][0]
                 todos = functions.get_todos()
                 todos.remove(todo_to_complete)
+                todos_completed.append(todo_to_complete)
                 functions.write_todos(todos)
                 window["todos"].update(values=todos)
                 window["todo"].update(value="")
+                todos = functions.get_todos()
             except IndexError:
                 sg.popup("Please select an item first.", font=("Helvetica", 20))
         case "todos":
@@ -63,7 +80,25 @@ while True:
             except IndexError:
                 sg.popup("Please add an item first.", font=("Helvetica", 20))
         case "Exit":
-            break
+            confirm = sg.popup_yes_no("Want you to save your progress before exiting?",
+                                      title="Exit confirmation",keep_on_top=True, modal=True, font=("Helvetica", 20))
+            if confirm == "Yes":
+                functions.write_todos(todos)
+                break
+            else:
+                functions.write_todos(todos_original)
+                break
+
+        case "Save":
+            confirm = sg.popup_yes_no("Are you sure you want to save your progress?",
+                              title="Exit confirmation", keep_on_top=True, modal=True, font=("Helvetica", 20))
+            todos = functions.get_todos()
+            if confirm == "Yes":
+                functions.write_todos(todos)
+            else:
+                pass
+
+
         case sg.WINDOW_CLOSED:
             break
 
